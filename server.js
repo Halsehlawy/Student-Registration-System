50fd029e0d124867272c530087496afcf73d08bd
Husain <halsehlawi@outlook.com>
2025-07-24 00:29:36 +0300

    DB connection, Authentication, Session

--- server.js
+++ server.js
@@ -7,20 +7,38 @@ const path = require('path');
 const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
 
-
+const authController = require('./controllers/auth');
 //SERVER
 const port = process.env.PORT ? process.env.PORT : '3000'
 const app = express();
 
-//MIDDLEWARE
+//DATABASE
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+// MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
+//SESSION
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 14 // 14 days
+    }
+}));
 //ROUTES
+app.use('/auth', authController)
 app.get('/', (req, res) => {
   res.render('index.ejs')
 });
+
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
